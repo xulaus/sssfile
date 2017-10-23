02d295fa280c6baece0beac85caefd27cb634190
Richard Fitzgerald <richard.fitzgerald@intellectionsoftware.com>
2017-10-23 14:55:47 +0100

    Move to cpp

--- src/python/setup.py
+++ src/python/setup.py
@@ -4,11 +4,16 @@
 from distutils.core import setup, Extension
 import numpy as np
 
-ext_modules = [Extension('sssfile', sources = ['bindings/main.c']) ]
+ext_modules = [Extension(
+    'sssfile',
+    sources = ['bindings/main.cpp'],
+    libraries = ['sssfile'],
+    extra_compile_args=["-O3"],
+)]
 
 setup(
     name = 'sssfile',
     version = '0',
-    include_dirs = [np.get_include()], #Add Include path of numpy
+    include_dirs = [np.get_include(), '../sssfile/include/'],
     ext_modules = ext_modules
     )
