5d387eef1c728d2fbb4b464d409d6f158b84cda8
Richard Fitzgerald <richard.fitzgerald@intellectionsoftware.com>
2017-11-29 18:08:05 +0000

    PEP8

--- src/python/setup.py
+++ src/python/setup.py
@@ -4,16 +4,16 @@
 from distutils.core import setup, Extension
 import numpy as np
 
-ext_modules = [Extension(
+EXT_MODULES = [Extension(
     'sssfile',
-    sources = ['bindings/main.cpp'],
-    libraries = ['sssfile'],
+    sources=['bindings/main.cpp'],
+    libraries=['sssfile'],
     extra_compile_args=["-O3"],
 )]
 
 setup(
-    name = 'sssfile',
-    version = '0',
-    include_dirs = [np.get_include(), '../sssfile/include/'],
-    ext_modules = ext_modules
-    )
+    name='sssfile',
+    version='0',
+    include_dirs=[np.get_include(), '../sssfile/include/'],
+    ext_modules=EXT_MODULES
+)
